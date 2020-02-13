45b81099d53fbe83314e783feb9e919ab3e89fad
khyoon <khyoon@fine-tec.com>
2020-02-13 17:25:34 +0900

    option patch start

---
+++ RPG_Style_Level_Up_Mod/Source/FP_RSLUM_mod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace FP_RSLUM
+{
+    class FP_RSLUM_mod : Verse.Mod
+    {
+
+        public static FP_RSLUM_setting Settings;
+
+        public FP_RSLUM_mod(ModContentPack content) : base(content)
+        {
+            FP_RSLUM_mod.Settings = GetSettings<ModSettings_Embrasures>();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "RPG_Style_Level_Up_Mod";
+            //return base.SettingsCategory();
+        }
+
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Settings.DoSettingsWindowContents(inRect);
+            //base.DoSettingsWindowContents(inRect);
+        }
+    }
+}
---
+++ RPG_Style_Level_Up_Mod/Source/FP_RSLUM_setting.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace FP_RSLUM
+{
+    class FP_RSLUM_setting : ModSettings
+    {
+
+        public int ColonistPercent = 75;
+        public int AnimalEXPPerTick = 1;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look<int>(ref ColonistPercent, "ColonistPercent", 75);
+            Scribe_Values.Look<int>(ref AnimalEXPPerTick, "AnimalEXPPerTick", 50);
+        }
+        
+        public void DoSettingsWindowContents(Rect canvas)
+        {
+            Listing_Standard _Listing_Standard = new Listing_Standard();
+            _Listing_Standard.ColumnWidth = 250f;
+            _Listing_Standard.Begin(canvas);
+            //listing_Standard.set_ColumnWidth(rect.get_width() - 4f);
+
+            _Listing_Standard.Label("Setting will only apply after a Game Restart.");
+            _Listing_Standard.Gap(12f);
+
+            _Listing_Standard.GapLine(12f);
+            _Listing_Standard.Label("Residents will get this % of their skill experience. : " + ColonistPercent + "%");
+            ColonistPercent = (int)(1000 * _Listing_Standard.Slider((float)(ColonistPercent) / 100f, 0.01f, 0.99f));
+
+            _Listing_Standard.Label("Sandbags are 65%");
+
+
+
+            _Listing_Standard.GapLine(12f);
+            _Listing_Standard.Label("Stuff Cost: " + StuffCost);
+            _Listing_Standard.IntAdjuster(ref StuffCost, 1, 1);
+            _Listing_Standard.Label("Wall is 5 stuff");
+
+            _Listing_Standard.End();
+        }
+    }
+
+}
