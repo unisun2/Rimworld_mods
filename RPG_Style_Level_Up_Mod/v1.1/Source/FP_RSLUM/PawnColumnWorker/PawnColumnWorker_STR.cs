b648627d83daccbc814a92b40f91e524d1c90424
khyoon <khyoon@fine-tec.com>
2020-02-25 18:21:57 +0900

    rpgmod 1.1

---
+++ RPG_Style_Level_Up_Mod/v1.1/Source/FP_RSLUM/PawnColumnWorker/PawnColumnWorker_STR.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace FP_RSLUM
+{
+    class PawnColumnWorker_STR : PawnColumnWorker
+    {
+        private static NumericStringComparer comparer = new NumericStringComparer();
+
+        protected virtual int Width
+        {
+            get
+            {
+                return this.def.width;
+            }
+        }
+
+        public override void DoCell(Rect rect, Pawn pawn, PawnTable table)
+        {
+            PawnLvComp pawnlvcomp = pawn.TryGetComp<PawnLvComp>();
+            if (pawnlvcomp != null)
+            {
+                if (pawnlvcomp.StatPoint > 0)
+                {
+                    this.DoStatUpButton(new Rect(rect.x, rect.yMin, 30f, 30f), pawn);
+                    Rect rect2 = new Rect(rect.x + 30, rect.y, rect.width - 30, Mathf.Min(rect.height, 30f));
+                    String textFor = this.GetTextFor(pawn);
+                    if (textFor != null)
+                    {
+                        Text.Font = GameFont.Small;
+                        Text.Anchor = TextAnchor.MiddleCenter;
+                        Text.WordWrap = false;
+                        Widgets.Label(rect2, textFor);
+                        Text.WordWrap = true;
+                        Text.Anchor = TextAnchor.UpperLeft;
+                        String tip = this.GetTip(pawn);
+                        if (!tip.NullOrEmpty())
+                        {
+                            TooltipHandler.TipRegion(rect2, tip);
+                        }
+                    }
+                }
+                else
+                {
+                    Rect rect2 = new Rect(rect.x + 15, rect.y, rect.width - 30, Mathf.Min(rect.height, 30f));
+                    String textFor = this.GetTextFor(pawn);
+                    if (textFor != null)
+                    {
+                        Text.Font = GameFont.Small;
+                        Text.Anchor = TextAnchor.MiddleCenter;
+                        Text.WordWrap = false;
+                        Widgets.Label(rect2, textFor);
+                        Text.WordWrap = true;
+                        Text.Anchor = TextAnchor.UpperLeft;
+                        String tip = this.GetTip(pawn);
+                        if (!tip.NullOrEmpty())
+                        {
+                            TooltipHandler.TipRegion(rect2, tip);
+                        }
+                    }
+                }
+            }
+        }
+
+        protected string GetTip(Pawn pawn)
+        {
+            return  "PawnColumnWorker_STR_Tip_Desc".Translate();
+        }
+
+
+
+        //protected override string GetTextFor(Pawn pawn)
+        //    => Math.Round(pawn.ageTracker.AgeBiologicalYearsFloat, 2).ToString("0.00");
+
+        protected string GetTextFor(Pawn pawn)
+        {
+            PawnLvComp pawnlvcomp = pawn.TryGetComp<PawnLvComp>();
+            if (pawnlvcomp != null)
+            {
+                return pawnlvcomp.STR.ToString();
+            }
+            else
+            {
+                // WHAT?
+                Log.Message("error in PawnColumnWorker_Level GetTextFor. no pawnlvcomp.");
+                return "err";
+            }
+        }
+
+        public override int Compare(Pawn a, Pawn b)
+        //    => a.ageTracker.AgeBiologicalYearsFloat.CompareTo(b.ageTracker.AgeBiologicalYearsFloat);
+        {
+            PawnLvComp pawnlvcompa = a.TryGetComp<PawnLvComp>();
+            PawnLvComp pawnlvcompb = b.TryGetComp<PawnLvComp>();
+            if (pawnlvcompa != null && pawnlvcompb != null)
+            {
+                return pawnlvcompa.STR.CompareTo(pawnlvcompb.STR);
+            }
+            else
+            {
+                // WHAT?
+                Log.Message("error in PawnColumnWorker_Level Compare. no pawnlvcomp.");
+                return 0;
+            }
+        }
+
+        public override int GetMinWidth(PawnTable table)
+            => base.GetMinWidth(table) + 20;
+
+        public void DoStatUpButton(Rect rect, Pawn pawn)
+        {
+            TooltipHandler.TipRegion(rect, Translator.Translate("LvTab_Distribute"));
+            if (Widgets.ButtonImage(rect, harmony_patches.DistributeIMG, Color.white, GenUI.SubtleMouseoverColor))
+            {
+                PawnLvComp pawnlvcomp = pawn.TryGetComp<PawnLvComp>();
+                if (pawnlvcomp != null)
+                {
+                    if (Input.GetKey(KeyCode.LeftShift))
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            if (pawnlvcomp.StatPoint > 0)
+                            {
+                                pawnlvcomp.StatPoint -= 1;
+                                pawnlvcomp.STR += 1;
+                            }
+                        }
+                    }
+                    else if (Input.GetKey(KeyCode.LeftControl))
+                    {
+                        for (int i = 0; i < 100; i++)
+                        {
+                            if (pawnlvcomp.StatPoint > 0)
+                            {
+                                pawnlvcomp.StatPoint -= 1;
+                                pawnlvcomp.STR += 1;
+                            }
+                        }
+                    }
+                    else if (Input.GetKey(KeyCode.LeftAlt))
+                    {
+                        if (pawnlvcomp.STR > FP_RSLUM_setting.Startingstat_min)
+                        {
+                            pawnlvcomp.StatPoint += 1;
+                            pawnlvcomp.STR -= 1;
+                        }
+                    }
+                    else
+                    {
+                        if (pawnlvcomp.StatPoint > 0)
+                        {
+                            pawnlvcomp.StatPoint -= 1;
+                            pawnlvcomp.STR += 1;
+                        }
+                    }
+                }
+                Hediff hediff = HediffMaker.MakeHediff(HediffDefOf.RSLUM_LVUP, pawn, null);
+                hediff.Severity = 0.1f;
+                pawn.health.AddHediff(hediff, null, null, null);
+            }
+        }
+    }
+}
---
+++ RPG_Style_Level_Up_Mod/v1.1/Source/FP_RSLUM/PawnLvComp.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace FP_RSLUM
+{
+    class PawnLvComp : ThingComp
+    {
+        public int level = -1;
+        public int exp = 0;
+        public int need_exp = 5000;
+        public int StatPoint = 1;
+        public int STR = -40;
+        public int DEX = -40;
+        public int AGL = -40;
+        public int CON = -40;
+        public int INT = -40;
+        public int CHA = -40;
+
+        public int exptick = 0;
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            if (this.level == -1)
+            {
+                if (FP_RSLUM_setting.FlatStartingStat)
+                {
+                    this.level = 0;
+                    STR = 0;
+                    DEX = 0;
+                    AGL = 0;
+                    CON = 0;
+                    INT = 0;
+                    CHA = 0;
+                }
+                else
+                {
+                    this.level += 1;
+                    STR = Rand.Range(FP_RSLUM_setting.Startingstat_min, FP_RSLUM_setting.Startingstat_max);
+                    DEX = Rand.Range(FP_RSLUM_setting.Startingstat_min, FP_RSLUM_setting.Startingstat_max);
+                    AGL = Rand.Range(FP_RSLUM_setting.Startingstat_min, FP_RSLUM_setting.Startingstat_max);
+                    CON = Rand.Range(FP_RSLUM_setting.Startingstat_min, FP_RSLUM_setting.Startingstat_max);
+                    INT = Rand.Range(FP_RSLUM_setting.Startingstat_min, FP_RSLUM_setting.Startingstat_max);
+                    CHA = Rand.Range(FP_RSLUM_setting.Startingstat_min, FP_RSLUM_setting.Startingstat_max);
+
+                }
+
+            }
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look<int>(ref this.level, "FP_RSLUM_level", 0, true);
+            Scribe_Values.Look<int>(ref this.exp, "FP_RSLUM_exp", 0, true);
+            Scribe_Values.Look<int>(ref this.need_exp, "FP_RSLUM_need_exp", 10000, true);
+            Scribe_Values.Look<int>(ref this.StatPoint, "FP_RSLUM_StatPoint", 1, true);
+            Scribe_Values.Look<int>(ref this.STR, "FP_RSLUM_STR", 0, true);
+            Scribe_Values.Look<int>(ref this.DEX, "FP_RSLUM_DEX", 0, true);
+            Scribe_Values.Look<int>(ref this.AGL, "FP_RSLUM_AGL", 0, true);
+            Scribe_Values.Look<int>(ref this.CON, "FP_RSLUM_CON", 0, true);
+            Scribe_Values.Look<int>(ref this.INT, "FP_RSLUM_INT", 0, true);
+            Scribe_Values.Look<int>(ref this.CHA, "FP_RSLUM_CHA", 0, true);
+        }
+
+        public bool canlevelup()
+        {
+            return exp > need_exp;
+        }
+
+        public void levelup()
+        {
+            bool needhediff = (exp > need_exp);
+
+            while (exp > need_exp)
+            {
+                this.level += 1;
+                this.StatPoint += 1;
+                exp -= need_exp;
+                need_exp = (int)Math.Ceiling(10000 * Math.Log(level + 1) * (1 + 0.01 * level));
+            }
+
+            if (needhediff)
+            {
+                Hediff hediff = HediffMaker.MakeHediff(HediffDefOf.RSLUM_LVUP, (Pawn)this.parent, null);
+                hediff.Severity = 0.1f;
+                ((Pawn)this.parent).health.AddHediff(hediff, null, null, null);
+            }
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (parent.def.race.Animal)
+            {
+                exptick++;
+                if (exptick > 600)
+                {
+                    exptick = 0;
+                    this.exp += FP_RSLUM_setting.AnimalEXPPerTick;
+                }
+            }
+        }
+
+
+    }
+}
