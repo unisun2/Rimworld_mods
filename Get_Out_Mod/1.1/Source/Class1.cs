6057f9be2e7589d8e6d0ca19843a96a1257baaa1
unisun2 <unisun2@naver.com>
2020-03-27 23:17:56 +0900

    bug fixed, burning slag

--- Get_Out_Mod/1.1/Source/Class1.cs
+++ Get_Out_Mod/1.1/Source/Class1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using HarmonyLib;
 using RimWorld;
 using RimWorld.QuestGen;
 using Verse;
@@ -29,6 +30,7 @@ namespace WG_GOM
 
         protected override bool TryExecuteWorker(IncidentParms parms)
         {
+            //parms.target = World;
             IncidentDef localDef;
             // vanila
 
@@ -100,97 +102,7 @@ namespace WG_GOM
 
 			// give quest - 1.1 ver!
 
-			
-
-			List<DebugMenuOption> list = new List<DebugMenuOption>();
-			float localPoints = parms.points ;
-			Slate testSlate = default(Slate);
-			Action<QuestScriptDef> generate = delegate (QuestScriptDef script)
-			{
-				List<DebugMenuOption> list2 = new List<DebugMenuOption>();
-				foreach (float item in DebugActionsUtility.PointsOptions(extended: false))
-				{
-					localPoints = item;
-					string text = item.ToString("F0");
-					testSlate = new Slate();
-					testSlate.Set("points", localPoints);
-					if (script != null)
-					{
-						if (script.IsRootDecree)
-						{
-							testSlate.Set("asker", PawnsFinder.AllMapsCaravansAndTravelingTransportPods_Alive_FreeColonists.RandomElement());
-						}
-						if (script == QuestScriptDefOf.LongRangeMineralScannerLump)
-						{
-							testSlate.Set("targetMineable", ThingDefOf.MineableGold);
-							testSlate.Set("worker", PawnsFinder.AllMaps_FreeColonists.FirstOrDefault());
-						}
-						if (!script.CanRun(testSlate))
-						{
-							text += " [not now]";
-						}
-					}
-					list2.Add(new DebugMenuOption(text, DebugMenuOptionMode.Action, delegate
-					{
-						int num = 0;
-						bool flag = script == null;
-						for (int i = 0; i < count; i++)
-						{
-							if (flag)
-							{
-								script = NaturalRandomQuestChooser.ChooseNaturalRandomQuest(localPoints, Find.CurrentMap);
-								Find.CurrentMap.StoryState.RecordRandomQuestFired(script);
-							}
-							if (script.IsRootDecree)
-							{
-								Pawn pawn = testSlate.Get<Pawn>("asker");
-								if (pawn.royalty.AllTitlesForReading.NullOrEmpty())
-								{
-									pawn.royalty.SetTitle(Faction.Empire, RoyalTitleDefOf.Knight, grantRewards: false);
-									Messages.Message("Dev: Gave " + RoyalTitleDefOf.Knight.label + " title to " + pawn.LabelCap, pawn, MessageTypeDefOf.NeutralEvent, historical: false);
-								}
-							}
-							if (count != 1 && !script.CanRun(testSlate))
-							{
-								num++;
-							}
-							else if (!logDescOnly)
-							{
-								QuestUtility.SendLetterQuestAvailable(QuestUtility.GenerateQuestAndMakeAvailable(script, testSlate));
-							}
-							else
-							{
-								Quest quest = QuestUtility.GenerateQuestAndMakeAvailable(script, testSlate);
-								Log.Message(quest.name + " (" + localPoints + " points)\n--------------\n" + quest.description + "\n--------------");
-								Find.QuestManager.Remove(quest);
-							}
-						}
-						if (num != 0)
-						{
-							Messages.Message("Dev: Generated only " + (count - num) + " quests.", MessageTypeDefOf.RejectInput, historical: false);
-						}
-					}));
-				}
-				Find.WindowStack.Add(new Dialog_DebugOptionListLister(list2));
-			};
-			list.Add(new DebugMenuOption("*Natural random", DebugMenuOptionMode.Action, delegate
-			{
-				generate(null);
-			}));
-			foreach (QuestScriptDef item2 in DefDatabase<QuestScriptDef>.AllDefs.Where((QuestScriptDef x) => x.IsRootAny))
-			{
-				QuestScriptDef localRuleDef = item2;
-				string defName = localRuleDef.defName;
-				list.Add(new DebugMenuOption(defName, DebugMenuOptionMode.Action, delegate
-				{
-					generate(localRuleDef);
-				}));
-			}
-			Find.WindowStack.Add(new Dialog_DebugOptionListLister(list.OrderBy((DebugMenuOption op) => op.label)));
-
-
-
-
+			AccessTools.Method(typeof(Verse.DebugActionsQuests), "GenerateQuests").Invoke(this, new object[] {1, false });
 
 
 			//localDef = DefDatabase<QuestScriptDef>.GetNamed("HuntingLodgeOppSW", false);
@@ -203,4 +115,27 @@ namespace WG_GOM
 
     }
 
+    
+    public class IncidentWorker_GetOutMod_quest : IncidentWorker
+    {
+
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            return true;
+        }
+
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            
+
+            // give quest - 1.1 ver!
+
+            AccessTools.Method(typeof(Verse.DebugActionsQuests), "GenerateQuests").Invoke(this, new object[] { 8, false });
+
+            return true;
+        }
+
+
+
+    }
 }
