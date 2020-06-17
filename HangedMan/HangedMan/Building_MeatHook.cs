e240b462759179ea8064907d4694e8dc90f0a7bf
khyoon <khyoon@fine-tec.com>
2020-06-17 18:30:57 +0900

    hangedman

---
+++ HangedMan/HangedMan/Building_MeatHook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace HangedMan
+{
+    class Building_MeatHook : Building_Bed
+    {
+        private List<Pawn> touchingPawns = new List<Pawn>();
+
+        public override void Tick()
+        {
+            base.Tick();
+            if (base.Spawned)
+            {
+                List<Thing> thingList = base.Position.GetThingList(base.Map);
+                for (int i = 0; i < thingList.Count; i++)
+                {
+                    Pawn pawn = thingList[i] as Pawn;
+                    if (pawn != null && !touchingPawns.Contains(pawn))
+                    {
+                        touchingPawns.Add(pawn);
+                        CheckSpring(pawn);
+                    }
+                }
+                for (int j = 0; j < touchingPawns.Count; j++)
+                {
+                    Pawn pawn2 = touchingPawns[j];
+                    if (!pawn2.Spawned || pawn2.Position != base.Position)
+                    {
+                        touchingPawns.Remove(pawn2);
+                    }
+                }
+            }
+            
+
+
+        }
+    }
+}
---
+++ HangedMan/HangedMan/HangedManDefOf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+
+namespace HangedMan
+{
+    [DefOf]
+    class HangedManDefOf
+    {
+        public static HediffDef HangedManHediff_Hooked;
+
+    }
+}
